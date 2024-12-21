588253e2b4fcd8e47f2a07c1e01e44579d15b496
ezequielleiter <ezequielleiter@gmail.com>
2024-12-21 11:56:38 -0300

    add user Id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const userId = process.env.USER_ID ? process.env.USER_ID : false;
+  let user = null;
+  if (userId) { 
+    const data = await fetch(`http://localhost:1337/api/users/${userId}`)
+    user = await data.json() 
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -14,11 +21,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+            {user?.description}
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
